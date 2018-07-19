5dfb0245eadc655ec797f26068da4613e26c8019
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2018-07-19 10:26:28 -0300

    Componente TabelaLista-Ação

--- resources/assets/js/components/TabelaLista.vue
+++ resources/assets/js/components/TabelaLista.vue
@@ -1,20 +1,31 @@
 <template>
   <div>
-    <a href="#">Criar</a>
+    <a v-if="criar" :href="criar">Criar</a>
     <table class="table table-striped table-hover">
       <thead>
-      <tr>
-        <th v-for="titulo of titulos">{{titulo}}</th>
-        <th>Ação</th>
-      </tr>
+        <tr>
+          <th v-for="titulo of titulos">{{titulo}}</th>
+          <th v-if="detalhe || editar || deletar">Ação</th>
+        </tr>
       </thead>
       <tbody>
-      <tr v-for="item of itens">
-        <td v-for="i of item">{{i}}</td>
-        <td>
-          <a href="#">Editar</a> | <a href="#">Deletar</a>
-        </td>
-      </tr>
+        <tr v-for="item of itens">
+          <td v-for="i of item">{{i}}</td>
+          <td v-if="detalhe || editar || deletar">
+            <form v-if="deletar && token" action="index.html" method="post">
+              <input type="hidden" name="_method" value="DELETE">
+              <input type="hidden" name="_token" :value="token">
+
+              <a v-if="detalhe" :href="detalhe">Detalhe |</a>
+              <a v-if="editar" :href="editar"> Editar |</a>
+              <a v-if="deletar" :href="deletar"> Deletar</a>
+            </form>
+
+            <a v-if="detalhe" :href="detalhe">Detalhe |</a>
+            <a v-if="editar" :href="editar"> Editar |</a>
+            <a v-if="deletar" :href="deletar"> Deletar</a>
+          </td>
+        </tr>
 
       </tbody>
     </table>
@@ -24,7 +35,14 @@
 
 <script>
   export default {
-    props:['titulos','itens'],
+    props:[ 'titulos',
+            'itens',
+            'criar',
+            'detalhe',
+            'editar',
+            'deletar',
+            'token'
+    ],
   }
 </script>
 
--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -6,7 +6,11 @@
             <tabela-lista
                 :titulos="['#','Título','Descrição','Data']"
                 :itens="[[1,'PHP OO','Curso de PHP OO','01/01/2018'],[2,'Vue JS','Curso de Vue JS','23/10/2017']]"
-
+                {{--criar="#criar"
+                detalhe="#detalhe"
+                editar="#editar"
+                deletar="#deletar"
+                token="234236457878"--}}
             >
             </tabela-lista>
         </painel>
