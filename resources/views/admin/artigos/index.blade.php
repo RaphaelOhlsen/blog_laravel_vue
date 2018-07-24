19394df5ed938dc49865957fc019c80e88dc2782
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2018-07-24 10:57:39 -0300

    Validação com Laravel

--- resources/views/admin/artigos/index.blade.php
+++ resources/views/admin/artigos/index.blade.php
@@ -2,6 +2,17 @@
 
 @section('content')
     <pagina tamanho="12">
+        @if($errors->all())
+            <div class="alert alert-danger alert-dismissible text-center" role="alert">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+
+                @foreach($errors->all() as $key => $value)
+                    <li><strong>{{$value}}</strong></li>
+                @endforeach
+            </div>
+        @endif
         <painel titulo="Lista de Artigos">
             <migalhas :lista="{{$listaMigalhas}}"></migalhas>
             <tabela-lista
@@ -19,19 +30,19 @@
         <formulario id="formAdicionar" css="" action="{{route('artigos.store')}}" method="post" enctype="" token="{{csrf_token()}}">
             <div class="form-group">
                 <label for="Titulo">Titulo</label>
-                <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título">
+                <input type="text" class="form-control" id="titulo" name="titulo" placeholder="Título" value="{{old('titulo')}}">
             </div>
             <div class="form-group">
                 <label for="descricao">Descrição</label>
-                <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição">
+                <input type="text" class="form-control" id="descricao" name="descricao" placeholder="Descrição" value="{{old('descricao')}}">
             </div>
             <div class="form-group">
                 <label for="conteudo">Conteudo</label>
-                <textarea class="form-control" name="conteudo" id="conteudo" cols="30" rows="10"></textarea>
+                <textarea class="form-control" name="conteudo" id="conteudo" cols="30" rows="10">{{old('conteudo')}}</textarea>
             </div>
             <div class="form-group">
                 <label for="data">Data</label>
-                <input type="datetime-local" class="form-control" id="data" name="data">
+                <input type="datetime-local" class="form-control" id="data" name="data" value="{{old('data')}}">
             </div>
         </formulario>
         <span slot="botoes"> <button form="formAdicionar" class="btn btn-info">Adicionar</button></span>
