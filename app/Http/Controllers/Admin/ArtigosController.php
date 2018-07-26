6ce76033639097020b10834edb3c845df8bf6ebe
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2018-07-26 17:25:58 -0300

    Exibir o Nome do Autor na Lista de Artigos

--- app/Http/Controllers/Admin/ArtigosController.php
+++ app/Http/Controllers/Admin/ArtigosController.php
@@ -22,19 +22,21 @@ class ArtigosController extends Controller
         ]);
 
         /*
-        $listaArtigos = Artigo::select('id','titulo','descricao','user_id','data')->paginate(5);
+              $listaArtigos = Artigo::select('id','titulo','descricao','user_id','data')->paginate(5);
 
-        foreach ($listaArtigos as $key => $value) {
-          $value->user_id = \App\User::find($value->user_id)->name;
-//          $value->user_id = $value->user->name;
-//          unset($value->user);
-        }*/
+              foreach ($listaArtigos as $key => $value) {
+                $value->user_id = \App\User::find($value->user_id)->name;
+      //          $value->user_id = $value->user->name;
+      //          unset($value->user);
 
-        $listaArtigos = DB::table('artigos')
-                        ->join('users','users.id','=','artigos.user_id')
-                        ->select('artigos.id','artigos.titulo','artigos.descricao','users.name','artigos.data')
-                        ->whereNull('deleted_at')
-                        ->paginate(5);
+
+              $listaArtigos = DB::table('artigos')
+                              ->join('users','users.id','=','artigos.user_id')
+                              ->select('artigos.id','artigos.titulo','artigos.descricao','users.name','artigos.data')
+                              ->whereNull('deleted_at')
+                              ->paginate(5);*/
+
+        $listaArtigos = Artigo::listaArtigos(5);
 
         return view('admin.artigos.index',compact('listaMigalhas','listaArtigos'));
     }
