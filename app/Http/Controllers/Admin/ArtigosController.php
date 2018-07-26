c6f0242dc3ee0543e495b09334005c65bc17cf9d
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2018-07-26 09:29:44 -0300

    Exibir o Nome do Autor na Lista de Artigos

--- app/Http/Controllers/Admin/ArtigosController.php
+++ app/Http/Controllers/Admin/ArtigosController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\Artigo;
 
@@ -20,7 +21,20 @@ class ArtigosController extends Controller
           ["titulo"=>"Lista de Artigos","url"=>""]
         ]);
 
+        /*
         $listaArtigos = Artigo::select('id','titulo','descricao','user_id','data')->paginate(5);
+
+        foreach ($listaArtigos as $key => $value) {
+          $value->user_id = \App\User::find($value->user_id)->name;
+//          $value->user_id = $value->user->name;
+//          unset($value->user);
+        }*/
+
+        $listaArtigos = DB::table('artigos')
+                        ->join('users','users.id','=','artigos.user_id')
+                        ->select('artigos.id','artigos.titulo','artigos.descricao','users.name','artigos.data')
+                        ->whereNull('deleted_at')
+                        ->paginate(5);
 
         return view('admin.artigos.index',compact('listaMigalhas','listaArtigos'));
     }
