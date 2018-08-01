c2632e292c913575166756f952a96a505ff3a73f
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2018-08-01 16:48:42 -0300

    Controle de Acesso

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,18 +7,22 @@
               <painel titulo="Dashboard">
                   <migalhas :lista="{{$listaMigalhas}}"></migalhas>
                   <div class="row">
-                      <div class="col-md-4">
-                          <caixa qtd="{{$totalArtigos}}" titulo="Artigos" url="{{route('artigos.index')}}" cor="orange" icone="ion ion-pie-graph"></caixa>
-                      </div>
-                      <div class="col-md-4">
-                          <caixa qtd="{{$totalUsuarios}}" titulo="Usuários" url="{{route('usuarios.index')}}" cor="blue" icone="ion ion-person-stalker"></caixa>
-                      </div>
-                      <div class="col-md-4">
-                          <caixa qtd="{{$totalAutores}}" titulo="Autores" url="{{route('autores.index')}}" cor="red" icone="ion ion-person"></caixa>
-                      </div>
-                      <div class="col-md-4">
-                          <caixa qtd="{{$totalAdmin}}" titulo="Administardaores" url="{{route('adm.index')}}" cor="green" icone="ion ion-person"></caixa>
-                      </div>
+                      @can('autor')
+                          <div class="col-md-4">
+                              <caixa qtd="{{$totalArtigos}}" titulo="Artigos" url="{{route('artigos.index')}}" cor="orange" icone="ion ion-pie-graph"></caixa>
+                          </div>
+                      @endcan
+                      @can('eAdmin')
+                          <div class="col-md-4">
+                              <caixa qtd="{{$totalUsuarios}}" titulo="Usuários" url="{{route('usuarios.index')}}" cor="blue" icone="ion ion-person-stalker"></caixa>
+                          </div>
+                          <div class="col-md-4">
+                              <caixa qtd="{{$totalAutores}}" titulo="Autores" url="{{route('autores.index')}}" cor="red" icone="ion ion-person"></caixa>
+                          </div>
+                          <div class="col-md-4">
+                              <caixa qtd="{{$totalAdmin}}" titulo="Administardaores" url="{{route('adm.index')}}" cor="green" icone="ion ion-person"></caixa>
+                          </div>
+                      @endcan
                   </div>
               </painel>
           </pagina>
