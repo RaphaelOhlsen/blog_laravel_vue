6a28685f786287b5fd75e4daae935e9b9c93fb77
Raphael Bernardo Ohlsen <raphael.ohlsen@gmail.com>
2018-08-01 17:00:16 -0300

    Considerações Finais

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -7,8 +7,8 @@
             <div class="row">
                 @foreach ($lista as $key => $value)
                     <artigocard
-                        titulo="{{$value->titulo}}"
-                        descricao="{{$value->descricao}}"
+                        titulo="{{str_limit($value->titulo,25,"...")}}"
+                        descricao="{{str_limit($value->descricao,40,"...")}}"
                         link="{{route('artigo', [$value->id, str_slug($value->titulo)])}}"
                         imagem="https://coletiva.net/files/e4da3b7fbbce2345d7772b0674a318d5/midia_foto/20170713/118815-maior_artigo_jul17.jpg"
                         data="{{$value->data}}"
